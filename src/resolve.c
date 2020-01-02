457055ec5f3cda19dc19a1c621680899dda2ebfe
Dylan Araps <dylan.araps@gmail.com>
2020-01-02 16:57:41 +0200

    eiwd: fix build

--- src/resolve.c
+++ src/resolve.c
@@ -170,7 +170,7 @@ void resolve_add_dns(uint32_t ifindex, uint8_t type, char **dns_list)
 	if (!dns_list || !*dns_list)
 		return;
 
-	if (!method.ops || !method.ops->add_domain_name)
+	if (!method.ops || !method.ops->add_dns)
 		return;
 
 	method.ops->add_dns(ifindex, type, dns_list, method.data);
